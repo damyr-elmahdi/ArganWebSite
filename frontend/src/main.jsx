75e443f11c239aacf3fa4de1a5c3a279388b56dd
mahdi damyr <mahdidamyr@gmail.com>
2025-04-17 09:51:22 +0100

    Login&register2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'  // Important: This imports the Tailwind styles
 
-createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-
+  </React.StrictMode>,
 )
